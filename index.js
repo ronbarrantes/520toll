ccb6721f752c2e2128c0d6a9953204d17689cc8f
ronbarrantes <ronbarrantes@gmail.com>
2019-05-02 00:04:57 -0700

    Add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const tollRouter = require('./api/routes/toll')
 
 const port = process.env.PORT || 3000
@@ -7,6 +8,7 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.use(require('morgan')('common'))
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
 app.use(express.static(`${__dirname}/dist`))
 app.use(tollRouter)
 app.get('*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
