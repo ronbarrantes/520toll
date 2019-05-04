84ec38969d589c5c0ef4a90bbe03b73b72aff8e7
ronbarrantes <ronbarrantes@gmail.com>
2019-05-03 23:17:29 -0700

    Add currentTimeInfo Selector

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchTollInfo } from '../actions/toll'
-import { tollData } from '../reducers/toll'
+import { todayInfo, todaySchedule, currentTimeInfo } from '../reducers'
 
 
-const App = ({ fetchTollInfo, toll }) => {
+const App = ({ fetchTollInfo, todaySchedule, todayInfo, currentTimeInfo }) => {
+
+  console.log('Current time -->',currentTimeInfo)
+
   useEffect(()=> {
     fetchTollInfo()
   }, [])
@@ -12,10 +15,11 @@ const App = ({ fetchTollInfo, toll }) => {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <p>{`Today is ${todayInfo.dayName || ''}`}</p>
       <ul>
-        {Object.keys(toll).map((spot, i) =>
+        {Object.keys(todaySchedule).map((spot, i) =>
           <li key={i}>
-            {toll[spot].schedule}
+            {todaySchedule[spot].schedule}
           </li>
         )}
       </ul>
@@ -24,7 +28,9 @@ const App = ({ fetchTollInfo, toll }) => {
 }
 
 const mapStateToProps = state => ({
-  toll: tollData(state),
+  todayInfo: todayInfo(state),
+  todaySchedule: todaySchedule(state),
+  currentTimeInfo: currentTimeInfo(state),
 })
 
 const mapDispatchToProps = {
